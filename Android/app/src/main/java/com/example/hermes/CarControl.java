8eb214b7f9131281c755c13f46e62e883a77a732
aminmah <aminmah@student.chalmers.se>
2022-04-06 14:32:22 +0200

    basic layout

---
+++ Android/app/src/main/java/com/example/hermes/CarControl.java
@@ -0,0 +1,59 @@
+package com.example.hermes;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.example.hermes.databinding.ActivityCarControlBinding;
+
+public class CarControl extends AppCompatActivity {
+
+    private AppBarConfiguration appBarConfiguration;
+    private ActivityCarControlBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        binding = ActivityCarControlBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        //setSupportActionBar(binding.toolbar);
+
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_car_control);
+        appBarConfiguration = new AppBarConfiguration.Builder(navController.getGraph()).build();
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+
+        binding.fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+    public void goUp(View view) {
+        Button b = findViewById(R.id.upButton);
+        System.out.println("go up!");
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_car_control);
+        return NavigationUI.navigateUp(navController, appBarConfiguration)
+                || super.onSupportNavigateUp();
+    }
+}
