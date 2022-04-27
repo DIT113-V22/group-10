3534b529583e7704c7f51b196e8d478b936a54e8
DanielJCoetzer <djcoetzer10@gmail.com>
2022-04-27 10:52:27 +0200

    #13 Changed delivery constructor which fixed CI

--- Android/app/src/main/java/com/example/hermes/Delivery.java
+++ Android/app/src/main/java/com/example/hermes/Delivery.java
@@ -10,17 +10,18 @@ public class Delivery {
     private final String customerID; //Change later to customer object
 
     public Delivery(String customerID){
-        this.ID = idGenerator();
         this.customerID = customerID;
         this.date = LocalDate.now().toString();
         this.time = LocalTime.now().toString();
+        this.ID = idGenerator();
     }
 
     public Delivery(String customerID, String date, String time){
-        this.ID = idGenerator();
+
         this.customerID = customerID;
         this.date = date;
         this.time = time;
+        this.ID = idGenerator();
     }
 
     public String getCustomerID(){
@@ -36,6 +37,6 @@ public class Delivery {
     }
 
     public int idGenerator(){
-        return (int) customerID.hashCode() * time.hashCode() * date.hashCode();
+        return (int) this.customerID.hashCode() * this.time.hashCode() * this.date.hashCode();
     }
 }
