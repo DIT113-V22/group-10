8b4cbefc42ae1add0df781c7762479194b0379fa
Emrik Dunvald <edunvald@gmail.com>
2022-05-25 13:38:16 +0300

    #14 Add dark theme to settings

--- Android/app/src/main/java/com/example/hermes/Settings.java
+++ Android/app/src/main/java/com/example/hermes/Settings.java
@@ -2,9 +2,13 @@ package com.example.hermes;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Switch;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.navigation.ui.AppBarConfiguration;
 
 import com.example.hermes.databinding.ActivitySettingsBinding;
@@ -29,7 +33,15 @@ public class Settings extends AppCompatActivity {
         Intent intent = new Intent(this, HomeScreen.class);
         startActivity(intent);
     }
+    public void darkTheme(View view){
+        boolean checked = ((Switch) view).isChecked();
+        if(checked)
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        else
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
-
+        Intent intent = new Intent(this,Settings.class);
+        startActivity(intent);
+    }
 }
 
