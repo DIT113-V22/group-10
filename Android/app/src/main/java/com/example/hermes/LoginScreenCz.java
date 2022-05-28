cc7762239f33e645b87e2f66998cd9e2471474ee
nihiliss <samcenko@student.chalmers.se>
2022-05-28 16:52:16 +0200

    #13 Add czech language screens

---
+++ Android/app/src/main/java/com/example/hermes/LoginScreenCz.java
@@ -0,0 +1,14 @@
+package com.example.hermes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginScreenCz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_screen_cz);
+    }
+}
---
+++ Android/app/src/main/java/com/example/hermes/SettingsCz.java
@@ -0,0 +1,14 @@
+package com.example.hermes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsCz extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_cz);
+    }
+}
