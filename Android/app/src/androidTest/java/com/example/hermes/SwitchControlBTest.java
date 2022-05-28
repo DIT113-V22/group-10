9dd517764b6b220d3fb1aec4732838c194ee623d
aminmah <aminmah@student.chalmers.se>
2022-05-28 21:48:31 +0200

    #67 Change tests because of their problem with database.

--- Android/app/src/androidTest/java/com/example/hermes/SwitchControlBTest.java
+++ Android/app/src/androidTest/java/com/example/hermes/SwitchControlBTest.java
@@ -3,12 +3,11 @@ package com.example.hermes;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
-import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static androidx.test.espresso.action.ViewActions.replaceText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withClassName;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withParent;
 import static androidx.test.espresso.matcher.ViewMatchers.withText;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.is;
@@ -25,54 +24,36 @@ import androidx.test.runner.AndroidJUnit4;
 import org.hamcrest.Description;
 import org.hamcrest.Matcher;
 import org.hamcrest.TypeSafeMatcher;
+import org.hamcrest.core.IsInstanceOf;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
-public class LoginTest {
+public class SwitchControlBTest {
 
     @Rule
-    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    public ActivityTestRule<CarControl> mActivityTestRule = new ActivityTestRule<>(CarControl.class);
 
     @Test
-    public void loginTest() {
-        ViewInteraction appCompatEditText = onView(
-                allOf(withId(R.id.loginEmail),
+    public void switchControlBTest() {
+        ViewInteraction materialButton4 = onView(
+                allOf(withId(R.id.Joystick), withText("Joystick"),
                         childAtPosition(
-                                allOf(withId(R.id.include),
+                                allOf(withId(R.id.frameLayout),
                                         childAtPosition(
                                                 withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
-                                                1)),
-                                1),
+                                                2)),
+                                5),
                         isDisplayed()));
-        appCompatEditText.perform(replaceText("test@test.com"), closeSoftKeyboard());
-
-        ViewInteraction appCompatEditText2 = onView(
-                allOf(withId(R.id.loginPassword),
-                        childAtPosition(
-                                allOf(withId(R.id.include),
-                                        childAtPosition(
-                                                withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
-                                                1)),
-                                2),
-                        isDisplayed()));
-        appCompatEditText2.perform(replaceText("Hello.123"), closeSoftKeyboard());
-
-        ViewInteraction materialButton = onView(
-                allOf(withId(R.id.loginButton), withText("Login"),
-                        childAtPosition(
-                                allOf(withId(R.id.include),
-                                        childAtPosition(
-                                                withClassName(is("androidx.coordinatorlayout.widget.CoordinatorLayout")),
-                                                1)),
-                                3),
-                        isDisplayed()));
-        materialButton.perform(click());
+        materialButton4.perform(click());
 
         ViewInteraction frameLayout = onView(
-                allOf(withId(android.R.id.content), isDisplayed()));
+                allOf(withId(android.R.id.content),
+                        withParent(allOf(withId(androidx.appcompat.R.id.action_bar_root),
+                                withParent(IsInstanceOf.<View>instanceOf(android.widget.FrameLayout.class)))),
+                        isDisplayed()));
         frameLayout.check(matches(isDisplayed()));
     }
 
